294d9e05dbf25efc795802d7b3ac819bf0175588
hrushikesh palave <hrushikesh palave@DESKTOP-4GAMNJ3>
2022-09-27 19:53:27 +0800

    fifth commit

---
+++ Day-5_Spring_Starter_Project/src/main/java/com/example/demo/Day5SpringStarterProjectApplication.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+
+@ComponentScan
+@Configuration
+@SpringBootApplication
+@EnableAutoConfiguration
+public class Day5SpringStarterProjectApplication {
+
+	public static void main(String[] args) {
+		
+		ConfigurableApplicationContext c=SpringApplication.run(Day5SpringStarterProjectApplication.class, args);        
+		Student s=new Student();
+		//Student s=c.getBean(Student.class);
+		s.display();
+		
+		
+		
+	}
+
+}
